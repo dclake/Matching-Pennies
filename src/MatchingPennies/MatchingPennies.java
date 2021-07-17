f95acd39aead1b4b0d33d87cc4da3f5063139961
dave.lake <dclake@gmail.com>
2021-07-17 12:04:47 -0400

    Added some comments and began work in starting Pennies input validation

--- src/MatchingPennies/MatchingPennies.java
+++ src/MatchingPennies/MatchingPennies.java
@@ -18,55 +18,60 @@ public class MatchingPennies {
 	public static void main(String[] args) {
 		//Declare Variables
 		char playerType, playerFlip, compFlip = 'T', compType;
-		String playerTypeChoice, userChoicePennies, playerFlipChoice,playerName = null, computerFlip = "T" ;
+		String playerTypeChoice, userChoicePennies, playerFlipChoice,playerName = null,compName=null, computerFlip = "T" ;
 		char []computerFlips= {'H', 'T'};
 		String pennyCountOutput, playerTypeOutput = null, flipChoiceOutput;
-		int playerPennies, compPennies, compFlipChoice, turnCount, minStart = 1, maxStart = 20;
+		int playerPennies =-1, compPennies = 0, compFlipChoice, turnCount, minStart = 1, maxStart = 20;
 		String winMessage = "You won this round", loseMessage = "You lost this round";
-		String wonGame = "You have won the game!!!", gameOver = "You have no more pennies to play. \n Game Over!!!";
+		String wonGame, gameOver;
+		
+		while (playerPennies < minStart) {
+			userChoicePennies = JOptionPane.showInputDialog("How many pennies would you like to start with? \nChoose a number between "
+					+minStart + " and " + maxStart);
+			//Parse userChoicePennies as a starting integer for both the player and the computer
+			playerPennies = Integer.parseInt(userChoicePennies);
+			compPennies = Integer.parseInt(userChoicePennies);
+		}
 		
 		
-		userChoicePennies = JOptionPane.showInputDialog("How many pennies would you like to start with? \nChoose a number between "
-														+minStart + " and " + maxStart);
 		
 		
-		//Parse userChoicePennies as a starting integer for both the player and the computer
-		playerPennies = Integer.parseInt(userChoicePennies);
-		compPennies = Integer.parseInt(userChoicePennies);
 		
 		
-		//Allow player to choose how they will play "Odd or even"
+		//Allows the player to choose how they will play "Odd or even" and sets name variables used in the rest of the program
 		playerTypeChoice = JOptionPane.showInputDialog("Which would you like to play as? \n [O]dd or [E]ven?");
-		
-		playerType = playerTypeChoice.charAt(0);//Parse playerTypeChoice as Char
-		
+		playerType = playerTypeChoice.charAt(0);
 		if (playerType == 'O' || playerType == 'o') {
 			compType = 'E';
 			playerName = "Odd";
-			//playerTypeOutput = "You have chosen to be the Odd player. Computer will be the Even player";
-		}
+			compName = "Even";		}
 		else
 			if (playerType == 'E' || playerType == 'e') {
 				compType = 'O';
 				playerName ="Even";
-				//playerTypeOutput = "You have chosen to be the Even player. Computer will be the Odd player";
+				compName = "Odd";
 			}
 
 		//Make the initial turn count equal to PlayerPennies since both player and computer have the same number of pennies
 		turnCount = playerPennies;
 		
-		
-		
+		//Loop through turn counts while these are more than zero and contains general game logic.
 		while (turnCount >0 ) {
 			pennyCountOutput = "Turn count is " + turnCount+ "\nHi " + playerName +" You have " + playerPennies + " pennies. "
 					+ "\nComputer has " + compPennies + " pennies.";
 			playerFlipChoice = JOptionPane.showInputDialog(pennyCountOutput+"\n\nLet's flip penny number " +".\n"
 					+ "H for Heads or T for Tails?"); 
-			playerFlip = playerFlipChoice.charAt(0); //Parse playerFlipChoice as Char
+			playerFlip = playerFlipChoice.charAt(0);
+			
+			//Randomly chooses the computers flip from an Array containing 'H' and 'T'
 			compFlipChoice = (int) (Math.random()*3);
 			compFlip = computerFlips[(int) (Math.random()*2)];
+			
+			
 			flipChoiceOutput = "You chose " +playerFlip + ".\nComputer chose " + compFlip + ".";
 			JOptionPane.showMessageDialog(null,flipChoiceOutput );
+			
+			//Logic for determining the winner of the round
 			if (playerFlip == compFlip && playerType == 'E') {
 				playerPennies++;
 				compPennies--;
@@ -84,6 +89,7 @@ public class MatchingPennies {
 				compPennies++;
 				JOptionPane.showMessageDialog(null,loseMessage );
 			}}
+				// Logic for setting the remaining turns in the loop by determining the player with the least pennies
 				if (playerPennies>compPennies) {
 					turnCount= compPennies;
 				}
@@ -93,10 +99,14 @@ public class MatchingPennies {
 				
 				}
 		
+		//Logic for determining the winner of the game.
 		if (playerPennies == 0) {
+			gameOver = "Sorry " + playerName + ". You have no more pennies to play.\n"
+					+ compName +" has won the game \nGame Over!!!";
 			JOptionPane.showMessageDialog(null, gameOver);
 		}
 		else {
+			wonGame = "WOW " + playerName +"!\n You are really good at this!\n You have won the game!!!";
 			JOptionPane.showMessageDialog(null, wonGame);
 
 		}
